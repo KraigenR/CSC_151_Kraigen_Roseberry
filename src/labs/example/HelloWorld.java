35235dbfa104695ef047b523313a5f93930f1754
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2026-01-28 10:29:54 -0500

    src example

---
+++ src/labs/example/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+Note: This program should reside in the directory path as follows-> CSC_151_Kraigen_Roseberry/src/labs/exammple/helloworld
+ */
+public class HelloWorld
+{
+        public static void main(String[] args)
+        {
+            System.out.println("Hello World");
+        }
+}

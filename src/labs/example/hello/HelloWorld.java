4eac84d776fec57dcd5161f040f037311d7ae43e
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2026-01-28 11:16:01 -0500

    In class Step 6 commit

--- src/labs/example/hello/HelloWorld.java
+++ src/labs/example/hello/HelloWorld.java
@@ -1,6 +1,9 @@
 /*
 Note: This program should reside in the directory path as follows-> CSC_151_Kraigen_Roseberry/src/labs/exammple/helloworld
  */
+
+package labs.example.hello;
+
 public class HelloWorld
 {
         public static void main(String[] args)

97359a5148d663f06a894c6bbe3dbe94df186597
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2026-02-16 11:43:10 -0500

    Clairifation of variables

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -4,25 +4,25 @@ public class Main {
  public static void main (String[] args)                                         {
     Physics physics = new Physics();
     //getting Distance 
-        double distance = physics.getDistance(50, 2);
+        double distance = physics.getDistance(50, 2);//Velocity and Time within the ()
         System.out.println("Distance is " + distance + " miles");
     //getting Velocity
-        double velocity = physics.getVelocity(100, 2);
+        double velocity = physics.getVelocity(100, 2);//distance and time
         System.out.println("Velocity is " + velocity + " mph");
     //getting Momentum
-        double momentum = physics.getMomentum(60, 2);
+        double momentum = physics.getMomentum(60, 2);//mass and velocity
         System.out.println("Momentum is " + momentum + " kg m/s");
     //getting Force
-        double force = physics.getForce(40, 2);
+        double force = physics.getForce(40, 2);//mass and acceleration
         System.out.println("Force is " + force + " kg m/s^2");
     //getting Work
-        double work = physics.getWork(70, 2);
+        double work = physics.getWork(70, 2);//force and distance
         System.out.println("Work is " + work + " Joules");
     //getting Kinetic Energy
-        double kineticEnergy = physics.getKineticEnergy(30, 2);
+        double kineticEnergy = physics.getKineticEnergy(30, 2);//mass and velocity
         System.out.println("Kinetic Energy is " + kineticEnergy + " Joules");
     //getting Potential Energy
-        double potentialEnergy = physics.getPotentialEnergy(30, 2);
+        double potentialEnergy = physics.getPotentialEnergy(30, 2);//mass and height
         System.out.println("Potential Energy is " + potentialEnergy + " Joules");
         public void trackangle(){
         int lengthone = (3);//first portion of the angle add your value here
@@ -32,4 +32,5 @@ public class Main {
         if (thetaDegrees < 36.87 | | thetaDegrees > 37.00) 
              {physics.logInvalidAngleInfo(thetaDegrees);}
         else { physics.logValidAngleInfo(thetaDegrees);}}
-                                                                                }
+    //
+                                                                                }}

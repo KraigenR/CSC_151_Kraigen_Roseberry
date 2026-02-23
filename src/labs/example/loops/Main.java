7065426c276e47fc0c039bc502903fa3e79cd4db
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2026-02-23 11:26:19 -0500

    Completed whileloop

---
+++ src/labs/example/loops/Main.java
@@ -0,0 +1,2 @@
+package labs.example.loops;
+public class Main 
--- src/labs/example/loops/WhileLoop.java
+++ src/labs/example/loops/WhileLoop.java
@@ -1,19 +1,23 @@
 package labs.example.loops;
 public class WhileLoop {
 
-    public static void main(String[] args){
+    public static void main(String[] args)  {
         WhileLoop loop = new WhileLoop();
-        loop.executeWhileLoop();}
+        loop.executeWhileLoop();
+                                            }
 
     private void executeWhileLoop(){
         int counter = 1; // starting value of counter 
         int iterations = 0; //how many iterations the loop has gone through
         while (counter <= 10){ 
             counter++;
-            iterations++;}
-                                    }
+            iterations++;
+                             }
+                                    
 System.out.println("The loop executed " + iterations + " times.");
 System.out.println("The loop is now complete.");
+                                   }
+                        }
             
 
 
